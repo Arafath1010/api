7c00703f7e822a11a31c066c41a09433a6584a6f
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-02-05 20:27:16 +0530

    Add files via upload

---
+++ search_result_api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from bs4 import BeautifulSoup
+import requests, lxml
+
+
+app = FastAPI()
+@app.post("/search/{keyword}")
+async def search(keyword):
+       l=[]
+       o={}
+       headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+       for i in range(0,100,10):
+           target_url="https://www.bing.com/search?q="+keyword+"&rdr=1&first={}".format(i+1)
+           #print(target_url)
+           resp=requests.get(target_url,headers=headers)
+           soup = BeautifulSoup(resp.text, 'html.parser')
+           completeData = soup.find_all("li",{"class":"b_algo"})
+           for i in range(0, len(completeData)):
+                o["Title"]=completeData[i].find("a").text
+                o["link"]=completeData[i].find("a").get("href")
+                o["Description"]=completeData[i].find("div", {"class":"b_caption"}).text
+                o["Position"]=i+1
+                l.append(o)
+                o={}
+       return {"results":l}
+# for run the api uvicorn search_result_api:app
