585dd48cb83774affc92ee1f41d4a5b8f8fb6a77
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-05-01 12:52:51 +0530

    Update search_result_api.py

--- search_result_api.py
+++ search_result_api.py
@@ -1,5 +1,11 @@
 #googletrans-3.1.0a0
 
+from typing import Optional
+from pydub import AudioSegment
+import speech_recognition as sr
+from gtts import gTTS
+from fastapi.responses import FileResponse
+
 from fastapi import FastAPI
 import googletrans
 from googletrans import Translator
@@ -21,6 +27,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/text-to-speech/{text}")
+def text_to_speech(text: str):
+    tts = gTTS(text)
+    audio_file = 'text_to_speech.mp3'
+    tts.save(audio_file)
+    return FileResponse(audio_file, media_type='audio/mpeg')
+
+@app.post("/speech-to-text/")
+def speech_to_text(audio_file: bytes, lang: Optional[str] = "en-US"):
+    recognizer = sr.Recognizer()
+    sound = AudioSegment.from_file(audio_file)
+    sound.export("audio.wav", format="wav")
+    with sr.AudioFile("audio.wav") as source:
+        audio = recognizer.record(source)
+    text = recognizer.recognize_google(audio, language=lang)
+    return {"text": text}
+
 
 @app.post("/translators/")
 async def tra(sentence,lang):
