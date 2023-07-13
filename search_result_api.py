495dabc35466c7927e4673bc9ebd04701167838d
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-07-13 12:29:35 +0530

    Update search_result_api.py

--- search_result_api.py
+++ search_result_api.py
@@ -68,53 +68,6 @@ wcapi = API(
 @app.post("/woo_make_order/")
 def make_order(data):
     print(type(data))
-    if data=="a":
-        data = {
-            "payment_method": "bacs",
-            "payment_method_title": "Direct Bank Transfer",
-            "set_paid": True,
-            "billing": {
-                "first_name": "Arafath",
-                "last_name": "Doe",
-                "address_1": "969 Market",
-                "address_2": "",
-                "city": "San Francisco",
-                "state": "CA",
-                "postcode": "94103",
-                "country": "US",
-                "email": "arafath.doe@example.com",
-                "phone": "(555) 555-5555"
-            },
-            "shipping": {
-                "first_name": "John",
-                "last_name": "Doe",
-                "address_1": "969 Market",
-                "address_2": "",
-                "city": "San Francisco",
-                "state": "CA",
-                "postcode": "94103",
-                "country": "US"
-            },
-            "line_items": [
-                {
-                    "product_id": 22150,
-                    "quantity": 2
-                },
-                {
-                    "product_id": 22120,
-                    "quantity": 1
-                }
-            ],
-            "shipping_lines": [
-                {
-                    "method_id": "flat_rate",
-                    "method_title": "Flat Rate",
-                    "total": "10.00"
-                }
-            ]
-        }
-    else:
-        data = data
     return wcapi.post("orders", data).json()
         
 
